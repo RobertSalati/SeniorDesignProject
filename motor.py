a5736a34654d374f8f96078f71d8fb50e25efb26
Rob <midnight62508@gmail.com>
2021-10-26 18:00:22 -0400

    wrote code

--- motor.py
+++ motor.py
@@ -1,16 +1,48 @@
 import picamera as cam;
 import numpy as np;
 from adafruit_motorkit import MotorKit;
+from adafruit_motor import stepper;
 
 l = 3;      # length of the shelf (x axis) [m]
 w = 1;      # width of the shelf (y axis)[m]
 h = 0.1;    # camera sag [m]
 
-def moveMotor():
-    # simply moves the motor. Nothing more nothing less
-    print("moveMotor()");
+kit1 = MotorKit(address=0x6f);
+kit2 = MotorKit(address=0x6f);
+
+def moveMotor(steps, num, direc):
+    if num == 1:            # Motor 1
+        if direc == 0:
+            for i in range(steps):
+                kit1.stepper1.onestep(direction=stepper.FORWARD);
+        if direc == 1:
+            for i in range(steps):
+                kit1.stepper1.onestep(direction=stepper.BACKWARD);
+     
+    if num == 2:           # Motor 2
+        if direc == 0:
+            for i in range(steps):
+                kit1.stepper2.onestep(direction=stepper.FORWARD);
+        if direc == 1:
+            for i in range(steps):
+                kit1.stepper2.onestep(direction=stepper.BACKWARD);
     
-    return 0;
+    if num == 3:            # Motor 3
+        if direc == 0:
+            for i in range(steps):
+                kit2.stepper1.onestep(direction=stepper.FORWARD);
+        if direc == 1:
+            for i in range(steps):
+                kit2.stepper1.onestep(direction=stepper.BACKWARD);
+    
+    if num == 4:            # Motor 4
+        if direc == 0:
+            for i in range(steps):
+                kit2.stepper2.onestep(direction=stepper.FORWARD);
+        if direc == 1:
+            for i in range(steps):
+                kit2.stepper2.onestep(direction=stepper.BACKWARD);
+
 
 def calcLength(x, y):
 
--- motorTest.py
+++ motorTest.py
@@ -2,11 +2,24 @@ from adafruit_motorkit import MotorKit;
 from adafruit_motor import stepper;
 import board;
 import time;
+from motor import *;
 
-kit = MotorKit(address=0x6f);   
+kit = MotorKit(address=0x6f);
+#kit1 = MotorKit(address=0x70);
 
-for i in range(1000):
+#2048 steps for 1 rotation with small motor
+#200 steps for 1 rotation with big motor
 
-    kit.stepper2.onestep();
-    time.sleep(0.01);
+# 360 = 2048
+
+
+while True:
+    angle = int(input("Move angle: "));
+    num = int(input("Motor number: "));
+    steps = abs(int(angle/360 * 2048));
+    if angle <= 0:
+        direction = 1;
+    else:
+        direction = 0;
+    moveMotor(steps,num,direction);
 
