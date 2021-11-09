c0fd95772ee9103f2235f02d67b1b9bf10b738fb
Rob <midnight62508@gmail.com>
2021-11-08 22:34:36 -0500

    updated motor

--- motor.py
+++ motor.py
@@ -7,58 +7,27 @@ l = 3;      # length of the shelf (x axis) [m]
 w = 1;      # width of the shelf (y axis)[m]
 h = 0.1;    # camera sag [m]
 
-global lengths;
-
-lengths = np.array([0, 0, 0, 0]);
-
 kit1 = MotorKit(address=0x60);
-kit2 = MotorKit(address=0x61);
+#kit2 = MotorKit(address=0x61);
+motorAddresses = [kit1.stepper1, kit1.stepper2];
 
-def moveMotor(steps, motorNum, dir):
-    if motorNum == 1:            # Motor 1
-        if dir == 0:
-            for i in range(steps):
-                kit1.stepper1.onestep(direction=stepper.FORWARD);
-        if dir == 1:
-            for i in range(steps):
-                kit1.stepper1.onestep(direction=stepper.BACKWARD);
-     
-    if motorNum == 2:           # Motor 2
-        if dir == 0:
-            for i in range(steps):
-                kit1.stepper2.onestep(direction=stepper.FORWARD);
-        if dir == 1:
-            for i in range(steps):
-                kit1.stepper2.onestep(direction=stepper.BACKWARD);
+class Motor:
+    length = 0;
+
+    def __init__(self,num, xpos, ypos):
+        self.num = num;
+        self.xpos = xpos;
+        self.ypos = ypos;
+
+
+    def moveMotor(self, steps, dir):
+
+        if (dir == 0):
+            dir = stepper.FORWARD;
+        elif (dir == 1):
+            dir = stepper.BACKWARD
+        for i in range(steps):
+            motorAddresses[self.num].onestep(direction=dir);
     
-    if motorNum == 3:            # Motor 3
-        if dir == 0:
-            for i in range(steps):
-                kit2.stepper1.onestep(direction=stepper.FORWARD);
-        if dir == 1:
-            for i in range(steps):
-                kit2.stepper1.onestep(direction=stepper.BACKWARD);
-    
-    if motorNum == 4:            # Motor 4
-        if dir == 0:
-            for i in range(steps):
-                kit2.stepper2.onestep(direction=stepper.FORWARD);
-        if dir == 1:
-            for i in range(steps):
-                kit2.stepper2.onestep(direction=stepper.BACKWARD);
-
-
-def calcLength(x, y):
-
-    print("calcLength()");
-
-    L1 = np.sqrt((l-x)**2+(w-y)**2+h**2);
-
-    L2 = np.sqrt((l-x)**2+(w+y)**2+h**2);
-
-    L3 = np.sqrt((l+x)**2+(w+y)**2+h**2);
-
-    L4 = np.sqrt((l+x)**2+(w-y)**2+h**2);
-
-    lengths = np.array([L1, L2, L3, L4]);
-    return lengths;
+    def changeLength(self,x,y):
+        self.length = np.sqrt((self.xpos-x)**2+(self.ypos-y)**2);
--- motorTest.py
+++ motorTest.py
@@ -3,9 +3,7 @@ from adafruit_motor import stepper;
 import board;
 import time;
 from motor import *;
-
-kit = MotorKit(address=0x6f);
-#kit1 = MotorKit(address=0x70);
+import numpy as np
 
 #2048 steps for 1 rotation with small motor
 #200 steps for 1 rotation with big motor
@@ -13,13 +11,17 @@ kit = MotorKit(address=0x6f);
 # 360 = 2048
 
 
+motors = np.array([Motor(0,1,1), Motor(1,-1,1), Motor(2,-1,-1), Motor(3,1,-1)]) 
+
 while True:
     angle = int(input("Move angle: "));
     num = int(input("Motor number: "));
-    steps = abs(int(angle/360 * 2048));
+    steps = abs(int(angle/360 * 200));
     if angle <= 0:
         direction = 1;
     else:
         direction = 0;
-    moveMotor(steps,num,direction);
+
+    motors[num-1].moveMotor(steps, direction);
+    
 
